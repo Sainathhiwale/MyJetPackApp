d5dd5aa9602380a15a77d9be0ce3ed1985f4cefc
sainathhiwale <sainathhiwale2@gmail.com>
2024-06-02 12:18:38 +0530

    image profile is created with surface ui

--- app/src/main/java/com/examen/myjetpackapp/MainActivity.kt
+++ app/src/main/java/com/examen/myjetpackapp/MainActivity.kt
@@ -3,13 +3,32 @@ package com.examen.myjetpackapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.examen.myjetpackapp.ui.theme.MyJetPackAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +50,42 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard(){
-   Text(text = "Hello Sainath")
+   Surface( modifier = Modifier
+       .fillMaxWidth()
+       .fillMaxHeight()){
+       Card(modifier = Modifier
+           .width(200.dp)
+           .height(390.dp)
+           .padding(12.dp),
+           shape = RoundedCornerShape(corner = CornerSize(15.dp))) {
+           Column (modifier = Modifier.height(300.dp),
+               verticalArrangement = Arrangement.Top,
+               horizontalAlignment = Alignment.CenterHorizontally){
+               CreatedImageProfile()
+           }
+
+       }
+   }
+}
+
+@Composable
+private fun CreatedImageProfile() {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        shadowElevation = 4.dp,
+        color = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.baseline_person_24),
+            contentDescription = "profile image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
 }
 
 
