91dba4b4703e3851afa1b136a9b06830c97aae69
sainathhiwale <sainathhiwale2@gmail.com>
2024-07-26 19:23:03 +0530

    note is updated

--- app/src/main/java/com/examen/myjetpackapp/MainActivity.kt
+++ app/src/main/java/com/examen/myjetpackapp/MainActivity.kt
@@ -2,12 +2,17 @@ package com.examen.myjetpackapp
 
 import android.os.Bundle
 import android.util.Log
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,6 +20,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -28,6 +35,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,9 +61,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard(){
-   Surface( modifier = Modifier
-       .fillMaxWidth()
-       .fillMaxHeight()){
+    var buttonClickedState = remember {
+        mutableStateOf(false)
+    }
+    Surface( modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()){
        Card(modifier = Modifier
            .width(200.dp)
            .height(390.dp)
@@ -68,16 +79,71 @@ fun CreateBizCard(){
                Divider()
                CreatedInfo()
                Button(onClick = {
-                   Log.d("clicked", "CreateBizCard: ")
+                   buttonClickedState.value = !buttonClickedState.value
+                   Log.d("MainActivity", "CreateBizCard: " + buttonClickedState.value)
                }) {
                    Text(text = "Portfolio",
-                       style = MaterialTheme.typography.bodyMedium)
+                       style = MaterialTheme.typography.labelSmall)
                }
+               if (buttonClickedState.value) {
+                   Content(isVisible = buttonClickedState.value)
+               } else {
+                   Box {} // Empty box for when content is hidden
+               }
+
            }
 
        }
    }
 }
+
+
+@Composable
+fun Content(isVisible: Boolean) {
+    Log.d("MainActivity", "Content: "+ isVisible)
+   if (isVisible){
+       Box(modifier = Modifier
+           .fillMaxHeight()
+           .fillMaxWidth()
+           .padding(5.dp)){
+           Surface (modifier = Modifier
+               .padding(3.dp)
+               .fillMaxWidth()
+               .fillMaxHeight(),
+               shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+               border = BorderStroke(width = 2.dp, color = Color.Red)){
+               Portfolio(data = listOf("Project 1",
+                   "Project 2",
+                   "Project 3",
+                   "Project 4",
+                   "Project 5"))
+           }
+       }
+   }
+
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+   LazyColumn{
+       items(data){item ->
+         Card(modifier = Modifier
+             .padding(13.dp)
+             .fillMaxHeight(),
+             shape = RectangleShape) {
+             Row (modifier = Modifier
+                 .padding(8.dp)
+                 .background(MaterialTheme.colorScheme.surface)
+                 .padding(16.dp)){
+                 CreatedImageProfile(modifier = Modifier.size(100.dp))
+                 Text(item)
+             }
+         }
+       }
+   }
+}
+
+
 
 @Composable
 private fun CreatedInfo() {
@@ -102,9 +168,9 @@ private fun CreatedInfo() {
 }
 
 @Composable
-private fun CreatedImageProfile() {
+private fun CreatedImageProfile(modifier: Modifier=Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
